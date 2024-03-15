533d376aeedf2485a4c14da72c92ff8ba11935ff
santiardila0615 <santiagoardila@colegioguanenta.edu.co>
2024-03-15 08:25:05 -0500

    se realizo analisis y diseño

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/d7s6gxoe)
 # Quiz instrucciones condicionales
 
 ### Realice el análisis, diseño y construcción para resolver el siguiente problema:
 
 Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo.
+# ANALISIS
 
+--Variables de entrada 
+a = lado de longitud a
+b = lado de longitud b
+c = lado de longitud c
+
+--variables de proceso
+
+a + b > c ,
+a + c > b ,
+b + c > a ,
+
+--Variables de salida
+
+Print : dice si son triangulos o no pueden serlo
 ## DISEÑO
 
 ### Diagrama de flujo
--- quiz.py
+++ quiz.py
@@ -1,12 +1,28 @@
-# Quiz instrucciones condicionales
+#Quiz instrucciones condicionales
 """Dados tres números a, b y c, correspondientes a la longitud de los lados de una figura geométrica, determinar si pueden formar los lados de un triángulo."""
-
+ 
 print("------------------------------")
 print("----- VERIFICAR TRIANGULO ----")
 print("------------------------------")
 
 # input
+a = float(input("Ingrese el valor de a: "))     
+b = float(input("Ingrese el valor de b "))
+c = float(input("Ingrse el valor de c "))
 
 # processing
+if a + b > c:
+    print("los numeros si forman un triangulo")
+elif a + c > b:
+    print("los numeros si forman un triangulo")
+elif b + c > a:
+    print("los numeros si forman un triangulo")
+else:
+    print("los numeros si forman un triangulo")
+if b + c > a:
+    print("los numeros si forman un triangulo")
+else:
+    print("los numeros si forman un triangulo")
+
 
 # output
